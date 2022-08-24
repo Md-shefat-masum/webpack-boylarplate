9b2f925caabc5d6dc8a45d0034b4fd2e44e14cf1
Md-shefat-masum <mshefat924@gmail.com>
2022-08-24 21:31:21 +0600

    html update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -97,7 +96,6 @@ const config = {
 module.exports = () => {
    if (isProduction) {
       config.mode = "production";
-
       // config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
    } else {
       config.mode = "development";
